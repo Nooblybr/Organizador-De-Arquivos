dec9a80be308218e4c16ad75cec836102ec995f4
Nooblybr <166092409+Nooblybr@users.noreply.github.com>
2025-02-01 11:16:45 -0300

    Verificar arquivos que já existem antes de mover

--- Program.cs
+++ Program.cs
@@ -3,13 +3,17 @@
     static void Main()
     {
         Console.WriteLine("Digite o caminho da pasta para ser organizada: ");
-        string caminhoDaPasta = Console.ReadLine();
+        string caminhoDaPasta = PegarCaminhoDaPasta();
         string pastaOrganizada = Path.Combine(caminhoDaPasta, "Arquivos Organizados");
-        string[] arquivos = Directory.GetFiles(caminhoDaPasta).ToArray();
-        MoverArquivos(pastaOrganizada, arquivos);
+
+        string[] arquivos = Directory.GetFiles(caminhoDaPasta);
+
+        
+        LoopPrincipal(pastaOrganizada, arquivos);
     }
 
-    static void MoverArquivos(string caminhoDaPastaOrganizada, string[] arquivos)
+
+    static void LoopPrincipal(string caminhoDaPastaOrganizada, string[] arquivos)
     {
         foreach (string arquivo in arquivos)
         {
@@ -17,19 +21,70 @@
             var nomeArquivo = Path.GetFileName(arquivo);
             string pastaDestino = Path.Combine(caminhoDaPastaOrganizada, extensao);
 
-            if (!Path.Exists(pastaDestino))
-                Directory.CreateDirectory(pastaDestino);
+            VerificarPastaExiste(pastaDestino);
 
-            try
-            {
-                File.Move(arquivo, Path.Combine(pastaDestino, nomeArquivo + extensao));
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine($"Não foi possível mover o arquivo {nomeArquivo}");
-                Console.ReadLine();
-            }
+            MoverArquivos(arquivo, pastaDestino, nomeArquivo, extensao);
 
         }
     }
+
+
+    static string PegarCaminhoDaPasta()
+    {
+        string caminho = Console.ReadLine();
+
+        while (!Directory.Exists(caminho)) 
+        {
+            Console.WriteLine("A pasta não existe." + Environment.NewLine + "Digite novamente:");
+            caminho = Console.ReadLine();
+        }
+
+        return caminho;
+    }
+
+
+    static void VerificarPastaExiste(string caminhoDaPasta)
+    {
+        if (!Directory.Exists(caminhoDaPasta))
+            Directory.CreateDirectory(caminhoDaPasta);
+    }
+
+
+    static void MoverArquivos(string arquivo, string pastaDestino, string nomeArquivo, string extensao)
+    {
+        string destino = Path.Combine(pastaDestino, nomeArquivo);
+        string destinoFinal = VerificarArquivoExiste(destino, extensao);
+        try
+        {
+            File.Move(arquivo, destinoFinal);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Não foi possível mover o arquivo {nomeArquivo}");
+            Console.ReadLine();
+        }
+    }
+
+    static string VerificarArquivoExiste(string destino, string extensao)
+    {
+        int i = 0;
+        string novoNome = "";
+
+        if (!File.Exists(destino))
+        {
+            return destino;
+        }
+        else
+        {
+            while (File.Exists(destino))
+            {
+                i++;
+                novoNome = destino.TrimEnd(extensao.ToCharArray());
+                destino = novoNome + $"({i})" + extensao;
+            }
+        }
+
+        return destino;
+    }
+
 }
